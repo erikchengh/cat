5f3add5dffdf0a309ae417b424a76eeb8ea409a7
erikchengh <122431764+erikchengh@users.noreply.github.com>
2026-01-16 12:48:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 from io import BytesIO
